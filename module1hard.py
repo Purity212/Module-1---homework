dfafe5c72ded0557d7191186c243142967a654ce
Purity212 <158211801+Purity212@users.noreply.github.com>
2024-05-14 01:09:57 +0300

    Итоговое задание Модуля 1

---
+++ module1hard.py
@@ -0,0 +1,32 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+#список из средних баллов
+average_grades = []
+for sublist in grades:
+    average_grades.append(sum(sublist)/len(sublist))
+
+#переход от множества к списку, чтобы отсортировать
+students = list(students)
+students.sort()
+#переход от списка к кортежу, т.к. в дальнейшем использую его как ключи для словаря (ключи дожны быть неименяемыми объектами)
+students = tuple(students)
+
+dictionary = {}
+
+dictionary.update(students[0])
+print(dictionary)
+
+
+
+
+#вспомогательный индекс, сопоставляю упорядоченные имена учеников с их средним баллом
+grades_index = 0
+for student in students:
+    dictionary[student] = average_grades[grades_index]
+    grades_index = grades_index + 1
+print(dictionary)
+
+
+
+
+
