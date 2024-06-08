3dd7b0fab0bee7405d48c886ec670e5bdb4f00b0
Purity212 <158211801+Purity212@users.noreply.github.com>
2024-06-09 02:49:43 +0300

    Модуль 3 - Домашняя работа по уроку "Пространство имен и способы вызова функции"

--- Module_3/homework1.py
+++ Module_3/homework1.py
@@ -1,13 +1,11 @@
-def test():
-    a = True
-    b = 10
-    print(a, b)
+def single_root_words(root_word, *other_words):
+    same_words = []
+    for i in other_words:
+        if root_word.lower() in i.lower() or i.lower() in root_word.lower():
+            same_words.append(i)
+    return same_words
 
-
-def test2(x, y, z):
-    print(x, y, z)
-
-
-test()
-print()
-test2(1, 2, 3)
+result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+print(result1)
+print(result2)
